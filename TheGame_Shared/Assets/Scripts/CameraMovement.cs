08d7e844389dc99189a24ce1a60c2a5927427de0
onewhit240sx <59681831+onewhit240sx@users.noreply.github.com>
2020-01-18 16:53:41 -0800

    camara
    follows player :) and stops at the end of the bushes

--- TheGame_Shared/Assets/Scripts/CameraMovement.cs
+++ TheGame_Shared/Assets/Scripts/CameraMovement.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
+    //camera variables so we can stop it from going out side the tile map
+    public Vector2 maxPosition;
+    public Vector2 minPosition; 
+
+    //smothing- for the camera moving with carector
+    //target sets the a target for the camera to follow 
+    public Transform target;
+    public float smoothing; 
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -11,8 +21,17 @@ public class CameraMovement : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        if(transform.position != target.position)
+        {
+            Vector3 targetPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
+
+            targetPosition.x = Mathf.Clamp(targetPosition.x, minPosition.x, maxPosition.x);
+            targetPosition.y = Mathf.Clamp(targetPosition.y, minPosition.y, maxPosition.y); 
+
+            transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing);
+
+        }
     }
 }
