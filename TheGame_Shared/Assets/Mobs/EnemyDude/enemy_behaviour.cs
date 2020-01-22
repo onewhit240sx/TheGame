b6a0d948bad6b712a5b9809185fb62d75f61ee96
HAHA <jperez5385@gmail.com>
2020-01-21 20:09:55 -0800

    added temp enemy

---
+++ TheGame_Shared/Assets/Mobs/EnemyDude/enemy_behaviour.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy_behaviour : MonoBehaviour
+{
+    public float mSpeed;
+    private Transform mTarget;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mTarget = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, mTarget.position, mSpeed * Time.deltaTime);
+    }
+}
