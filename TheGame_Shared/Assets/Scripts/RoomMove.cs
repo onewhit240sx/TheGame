8fd21216f266b1485713efeee970800068677150
onewhit240sx <59681831+onewhit240sx@users.noreply.github.com>
2020-01-21 21:08:43 -0800

    new place
    move to diferent place, go strait up

---
+++ TheGame_Shared/Assets/Scripts/RoomMove.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomMove : MonoBehaviour
+{
+
+    public Vector2 cameraChangeMaxPosition;
+    public Vector2 cameraChangeMinPosition;
+    public Vector3 playerChange;
+
+    private CameraMovement camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = Camera.main.GetComponent<CameraMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            camera.minPosition = cameraChangeMinPosition;
+            camera.maxPosition = cameraChangeMaxPosition;
+            other.transform.position += playerChange; 
+        }
+    }
+}
