c808536686235b10d748387d3bc6ec13b1aaadfe
HAHA <jperez5385@gmail.com>
2020-01-29 20:17:02 -0800

    Modified enemy dude

--- TheGame_Shared/Assets/Mobs/EnemyDude/enemy_behaviour.cs
+++ TheGame_Shared/Assets/Mobs/EnemyDude/enemy_behaviour.cs
@@ -4,61 +4,34 @@ using UnityEngine;
 
 public class enemy_behaviour : MonoBehaviour
 {
-    public float mSpeed;
-    private Transform mTarget;
-    private Vector2 change;
-    public CircleCollider2D attackRange;
+    public float speed;
+    public float idleRange;
+    public float beginAttackRange;
 
-   
-    
-
+    public Transform target;
     public Animator animator; 
 
-
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         animator = GetComponent<Animator>();
-        attackRange = GetComponent<CircleCollider2D>(); 
-
-       
-        mTarget = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        change = Vector2.zero;
-        transform.position = Vector2.MoveTowards(transform.position, mTarget.position, mSpeed * Time.deltaTime);
-        change.x = transform.position.x - mTarget.position.x;
-        change.y = transform.position.y - mTarget.position.y; 
-        
-    
-        
-
-        //makes the enemy move by changeing the boolean in the animator to true
-        if(change != Vector2.zero)
+    void Update() {
+        if(Vector2.Distance(transform.position, target.position) < idleRange)
         {
-            animator.SetFloat("moveX", change.x);
-            animator.SetFloat("moveY", change.y);
-            animator.SetBool("moving", true); 
+            animator.SetBool("isFollowing", true);
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
 
+            if (Vector2.Distance(transform.position, target.position) < beginAttackRange)
+            {
+
+            }
         }
         else
         {
-            animator.SetBool("moving",false); 
+            animator.SetBool("isFollowing", false);
         }
-        
-
-    }
-
-    private void OnTriggerEnter2D(CircleCollider2D collision)
-    {
-        if(collision.CompareTag("Player"))
-        {
-            animator.SetTrigger("New Triger"); 
-        }
-
-
     }
 }
