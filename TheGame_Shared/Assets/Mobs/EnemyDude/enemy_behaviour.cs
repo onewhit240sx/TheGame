226dfb986d0b4ea8fb94729002c1b4df279d0b7a
HAHA <jperez5385@gmail.com>
2020-02-02 15:03:24 -0800

    about to rewrite

--- TheGame_Shared/Assets/Mobs/EnemyDude/enemy_behaviour.cs
+++ TheGame_Shared/Assets/Mobs/EnemyDude/enemy_behaviour.cs
@@ -6,8 +6,9 @@ public class enemy_behaviour : MonoBehaviour
 {
     public float speed;
     public float idleRange;
-    public float beginAttackRange;
+    public float diveRange;
 
+    public Rigidbody2D rigidBody;
     public Transform target;
     public Animator animator; 
 
@@ -15,6 +16,7 @@ public class enemy_behaviour : MonoBehaviour
     void Start() {
         animator = GetComponent<Animator>();
         target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        rigidBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -24,14 +26,39 @@ public class enemy_behaviour : MonoBehaviour
             animator.SetBool("isFollowing", true);
             transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
 
-            if (Vector2.Distance(transform.position, target.position) < beginAttackRange)
+            if (Vector2.Distance(transform.position, target.position) < diveRange)
             {
+                animator.SetBool("isStriking", true);
 
+                if (Vector2.Distance(transform.position, target.position) == 0)
+                {
+                    // TODO: ADD MOB ATTACK
+                }
+            }
+            else
+            {
+                animator.SetBool("isStriking", false);
             }
         }
         else
         {
             animator.SetBool("isFollowing", false);
         }
+    }
+
+    public void KnockBack()
+    {
+        Debug.Log("Hit");
+
+        animator.SetBool("isStriking", false);
+        animator.SetBool("isFollowing", false);
+        StartCoroutine(getThrown());
+        rigidBody.AddForce(transform.forward * 500, ForceMode2D.Impulse);
+
+    }
+
+    IEnumerator getThrown()
+    {
+        yield return new WaitForSeconds(.3f);
     }
 }
